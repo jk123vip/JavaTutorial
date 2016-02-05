329c77458130441c01d5c79f47e94b88d30e9647
jk123vip <jk123vip@gmail.com>
2016-02-06 01:09:38 +0800

    add 17.DateAndTime

---
+++ 17.DateAndTime/DateDemo.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class DateDemo {
+
+    public static void main(String[] args) {
+
+        // 初始化Date对象
+        Date date = new Date();
+
+        // 使用 toString() 函数显示日期时间
+        System.out.println(date.toString());
+
+    }
+}
