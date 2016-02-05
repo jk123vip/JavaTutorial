e6bd350b4e9064f8c9424365c7ed42edabcbeb22
jk123vip <jk123vip@gmail.com>
2016-02-06 01:16:24 +0800

    SimpleDateFormat

--- 17.DateAndTime/DateDemo.java
+++ 17.DateAndTime/DateDemo.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.text.*;
 
 public class DateDemo {
 
@@ -6,9 +7,14 @@ public class DateDemo {
 
         // 初始化Date对象
         Date date = new Date();
-
         // 使用 toString() 函数显示日期时间
         System.out.println(date.toString());
+
+        // 使用SimpleDateFormat格式化日期
+        Date dNow = new Date();
+        SimpleDateFormat ft =
+            new SimpleDateFormat("E yyyy.MM.dd 'at' hh:mm:ss a zz");
+        System.out.println("Current Date: " + ft.format(dNow));
 
     }
 }
