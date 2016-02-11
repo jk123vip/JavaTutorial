a3b499e52cd764491a15be7224a7922fbcf85eef
jk123vip <jk123vip@gmail.com>
2016-02-11 16:05:37 +0800

    控制台输出

---
+++ 20.StreamFileIO/WriteDemo.java
@@ -0,0 +1,16 @@
+/** 控制台输出 */
+
+import java.io.*;
+
+// 演示System.out.write()
+public class WriteDemo {
+
+    public static void main(String[] args) {
+
+        int b;
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+
+    }
+}
