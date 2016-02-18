75c023a2296517ea07094f1a43172d243145b499
jk123vip <jk123vip@gmail.com>
2016-02-18 14:04:01 +0800

    Thread方法演示

---
+++ 35.多线程/DisplayMessage.java
@@ -0,0 +1,15 @@
+/** 通过实现Runnable接口创建线程 */
+
+public class DisplayMessage implements Runnable {
+
+    private String message;
+    public DisplayMessage(String message) {
+        this.message = message;
+    }
+    public void run() {
+        while (true) {
+            System.out.println(message);
+        }
+    }
+
+}
