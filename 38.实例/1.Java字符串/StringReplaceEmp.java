e0af808271c4f368364f3b6d6bb1816bad05aeb8
jk123vip <jk123vip@gmail.com>
2016-02-20 10:49:28 +0800

    字符串替换

---
+++ 38.实例/1.Java字符串/StringReplaceEmp.java
@@ -0,0 +1,11 @@
+/**
+ * 字符串替换。
+ */
+public class StringRemplaceEmp {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(str.replace("H", "W"));
+        System.out.println(str.replaceFirst("He", "Wa"));
+        System.out.println(str.replaceAll("He", "Ha"));
+    }
+}
