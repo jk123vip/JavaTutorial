169dbcfc7d226c0f30a720be53dc81302503b2cb
jk123vip <jk123vip@gmail.com>
2016-02-21 13:34:23 +0800

    数组合并

---
+++ 38.实例/2.Java数组/Main3.java
@@ -0,0 +1,20 @@
+/**
+ * 数组合并
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        String[] a = { "A", "E", "I" };
+        String[] b = { "O", "U" };
+        List list = new ArrayList(Arrays.asList(a));
+        list.addAll(Arrays.asList(b));
+        Object[] c = list.toArray();
+        System.out.println(Arrays.toString(c));
+    }
+
+}
