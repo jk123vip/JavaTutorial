9115ea588d306b92850fa7d717c5384d227c6e54
jk123vip <jk123vip@gmail.com>
2016-02-21 13:11:27 +0800

    数组输出

---
+++ 38.实例/2.Java数组/Welcome.java
@@ -0,0 +1,17 @@
+/**
+ * 数组输出
+ */
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        String[] greeting = new String[3];
+        greeting[0] = "This is the greeting";
+        greeting[1] = "for all the readers from";
+        greeting[2] = "Java Source.";
+        for (int i = 0; i < greeting.length; i ++) {
+            System.out.println(greeting[i]);
+        }
+    }
+
+}
