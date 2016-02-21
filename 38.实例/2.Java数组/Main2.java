c4bf24a5de3675f3c33411d339f687b92955940e
jk123vip <jk123vip@gmail.com>
2016-02-21 13:16:34 +0800

    数组获取最大和最小值

---
+++ 38.实例/2.Java数组/Main2.java
@@ -0,0 +1,18 @@
+/**
+ * 数组获取最大和最小值
+ */
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Integer[] numbers = {8, 2, 7, 1, 4, 9, 5};
+        int min = (int) Collections.min(Arrays.asList(numbers));
+        int max = (int) Collections.max(Arrays.asList(numbers));
+        System.out.println("最小值" + min);
+        System.out.println("最大值" + max);
+    }
+
+}
