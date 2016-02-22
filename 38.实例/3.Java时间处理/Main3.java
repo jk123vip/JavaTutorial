01dc01e074bc2f332f2bf83a7c68f9a9668bca6f
jk123vip <jk123vip@gmail.com>
2016-02-22 16:49:29 +0800

    时间戳转换成时间

---
+++ 38.实例/3.Java时间处理/Main3.java
@@ -0,0 +1,17 @@
+/**
+ * 时间戳转换成时间
+ */
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        Long timeStamp = System.currentTimeMillis(); // 获取当前时间戳
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String sd = sdf.format(new Date(Long.parseLong(String.valueOf(timeStamp)))); // 时间戳转换成时间
+        System.out.println(sd);
+    }
+
+}
