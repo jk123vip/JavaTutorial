fa3d6ba48dbd5df28601b93435ae3d9d623eec7a
jk123vip <jk123vip@gmail.com>
2016-02-23 20:17:56 +0800

    创建文件

---
+++ 38.实例/5.Java文件操作/Main1.java
@@ -0,0 +1,23 @@
+/**
+ * 创建文件
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("/home/jk/myfile.txt");
+            if (file.createNewFile()) {
+                System.out.println("文件创建成功");
+            } else {
+                System.out.println("出错了，该文件已经存在");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
