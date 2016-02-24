bf1ad4a16e1f4ffd8e50abbe19eee62ca5435af5
jk123vip <jk123vip@gmail.com>
2016-02-25 01:16:53 +0800

    遍历指定目录下的所有目录

---
+++ 38.实例/6.Java目录操作/Main10.java
@@ -0,0 +1,29 @@
+/**
+ * 遍历指定目录下的所有目录
+ */
+
+import java.io.*;
+
+public class Main10 {
+
+    public static void main(String[] args) {
+        File dir = new File("/home/jk/Documents");
+        File[] files = dir.listFiles();
+        FileFilter fileFilter = new FileFilter() {
+            public boolean accept(File file) {
+                return file.isDirectory();
+            }
+        };
+        files = dir.listFiles(fileFilter);
+        System.out.println(files.length);
+        if (files.length == 0) {
+            System.out.println("目录不存在或它不是一个目录");
+        } else {
+            for (int i = 0; i < files.length; i++) {
+                File filename = files[i];
+                System.out.println(filename.toString());
+            }
+        }
+    }
+
+}
