55f239f43c3567b793c36a425959760bc12572ca
jk123vip <jk123vip@gmail.com>
2016-02-29 20:53:02 +0800

    获取链表的元素

---
+++ 38.实例/8.Java数据结构/Main5.java
@@ -0,0 +1,27 @@
+/**
+ * 获取链表的元素
+ */
+
+import java.util.*;
+
+public class Main5 {
+    private LinkedList list = new LinkedList();
+    public void push(Object v) {
+        list.addFirst(v);
+    }
+    public Object top() {
+        return list.getFirst();
+    }
+    public Object pop() {
+        return list.removeFirst();
+    }
+    public static void main(String[] args) {
+        Main5 stack = new Main5();
+        for (int i = 30; i < 40; i++)
+            stack.push(new Integer(i));
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
