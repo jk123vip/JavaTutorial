88a9c3a651dd92737148639b388c880b9cf1ce55
jk123vip <jk123vip@gmail.com>
2016-03-12 23:14:07 +0800

    使用Enumeration遍历HashTable

---
+++ 38.实例/9.Java集合/Main14.java
@@ -0,0 +1,18 @@
+/**
+ * 使用Enumeration遍历HashTable
+ */
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class Main14 {
+    public static void main(String[] args) {
+        Hashtable ht = new Hashtable();
+        ht.put("1", "One");
+        ht.put("2", "Two");
+        ht.put("3", "Three");
+        Enumeration e = ht.elements();
+        while (e.hasMoreElements())
+            System.out.println(e.nextElement());
+    }
+}
